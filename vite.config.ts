c08d7d6f2976b635c95a442fc46fa3a642569b43
leadermalang <hassanali5120@gmail.com>
2024-05-27 00:21:52 -0700

    source map added in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default {
             name: 'test',
             fileName: 'index',
         },
+        sourcemap: true,
         rollupOptions: {
             external: ['react', 'react-dom'],
             output: {
