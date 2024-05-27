e67b1b2f965b59f71f07a2a07edf0d9e0588eaf5
leadermalang <hassanali5120@gmail.com>
2024-05-27 07:08:39 -0700

    source map false

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default {
             name: 'test',
             fileName: 'index',
         },
-        sourcemap: true,
+        sourcemap: false,
         rollupOptions: {
             external: ['react', 'react-dom'],
             output: {
